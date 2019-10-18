7b8d5d72261838f473ddaed7d3446c64b09c4b26
Fernando Jarilla <fjarilla@gmail.com>
2019-10-18 09:50:53 +0200

    System finished. Only the test are missing

--- timeBikeIsCycling.ino
+++ timeBikeIsCycling.ino
@@ -8,6 +8,8 @@
 
 #include <Wire.h>
 #include <ArduinoBLE.h>
+#include <Arduino_LSM9DS1.h>
+#include <Arduino_APDS9960.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,40 +19,43 @@
 //for each digit of the counter. We can count from 0 to 99.999.999 seconds without reset the counter.
 // Char0 Char1 Char2 Char3 Char4 Char5 Char6 Char7
 // MSB   ...   ...   ...   ...   ...   ...   LSB      (Counter)
-BLEService bikeService("19B10000-E8F2-537E-4F6C-D104768A1214");
-BLECharCharacteristic bikeCharacteristic0("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic1("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic2("19B10003-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic3("19B10004-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic4("19B10005-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic5("19B10006-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic6("19B10007-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLECharCharacteristic bikeCharacteristic7("19B10008-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEService bikeService("0000");
+BLECharCharacteristic bikeCharacteristic0("0001", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic1("0002", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic2("0003", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic3("0004", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic4("0005", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic5("0006", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic6("0007", BLERead | BLEWrite);
+BLECharCharacteristic bikeCharacteristic7("0008", BLERead | BLEWrite);
 
 //Global constants
 //set counter
 unsigned long timeCounter;
 char timeCounterString[8];
-//input for mouvement sensor
-int isMoving = 4;
-//value of mouvement sensor
-int valueOfIsMoving = 0;
+//inputs for mouvement sensor
+float x, y, z; 
+float antX=0, antY=0, antZ=0;
 
 void setup(){
-  //set pins as inputs
-  //mouvement sensor
-  pinMode(isMoving, INPUT);
-
   //set monitor
-  Serial.begin(115200);
-  while(!Serial);
+  //Serial.begin(115200);
+  //while(!Serial);
 
   //Bluetooth module
   //Begin initialization
   if (!BLE.begin()) {
-    Serial.println("Failed to start BLE");
+    //Serial.println("Failed to start BLE");
     while(1);
   }
+
+  //IMU module
+  //Begin initialization
+  if (!IMU.begin()) {
+    //Serial.println("Failed to initialize IMU");
+    while(1);
+  }
+  
   //Set advertised local name and service UUID:
   BLE.setLocalName("BIKE");
   BLE.setAdvertisedService(bikeService);
@@ -71,28 +76,25 @@ void setup(){
   //Assing event handlers for chracteristic. We handle the characteristic with the last characteristic
   //envoyed.
   //Now is not necessary. The phone read the value when it want
-  //bikeCharacteristic7.setEventHandler(BLEWritten, bikeCharacteristicWritten);
+  bikeCharacteristic7.setEventHandler(BLEWritten, bikeCharacteristicWritten);
 
   //Start advertising
   BLE.advertise();
 
   //Inform that module is initialized.
-  Serial.println("Bluetooth device active. Waiting for connections ...");
+  //Serial.println("Bluetooth device active. Waiting for connections ...");
+  //Serial.println("IMU device active.");
 }//end setup()
 
 //Activation with a characteristic is written. We dont do anything in this case. We write the characteristics
 //value every second, not when change
 //Not necessary at this moment
-/*
+
 void bikeCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
   //New value is wrotten to characteristic
-  Serial.print("timeCounterAntes: ");
-  Serial.println(timeCounter);
   readTimeCounter();
-  Serial.print("timeCounterDespues: ");
-  Serial.println(timeCounter); 
 }
-*/
+
 
 //Reading of time counter. We read the characteristics and put the value in int in the counter
 void readTimeCounter() {
@@ -103,7 +105,7 @@ void readTimeCounter() {
   timeCounterString[3] = bikeCharacteristic4.value();
   timeCounterString[2] = bikeCharacteristic5.value();
   timeCounterString[1] = bikeCharacteristic6.value();
-  timeCounterString[0] = bikeCharacteristic7.value();
+  timeCounterString[0] = bikeCharacteristic7.value();        
   timeCounter = atol(timeCounterString);
 }
 
@@ -120,7 +122,7 @@ void writeTimeCounterString() {
   bikeCharacteristic6.writeValue(timeCounterString[1]);
   bikeCharacteristic7.writeValue(timeCounterString[0]);
 }
- 
+
 void loop(){  
   //Listen for BLE peripherals to connect:
   BLEDevice central = BLE.central();
@@ -129,33 +131,44 @@ void loop(){
     central.poll();
     Serial.print("Connected to central: ");
     //Print the central's MAC address:
-    Serial.println(central.address());
+    //Serial.println(central.address());
     readTimeCounter();
-    Serial.print("timeCounterInLoop: ");
-    Serial.println(timeCounter);
+    //Serial.print("timeCounterInLoop: ");
+    //Serial.println(timeCounter);
     //While the central is still connected to peripheral:
     while (central.connected()) {
       //We don't do nothing at this moment. It isn't necessary
     }
     //When the central disconnects, print it out:
-    Serial.print(F("Disconnected from central: "));
-    Serial.println(central.address());
+    //Serial.print(F("Disconnected from central: "));
+    //Serial.println(central.address());
   }
   //The central is disconnected. We increment the counter every second if the bike is cycling.
   //Is mouvement sensor activate?
-  valueOfIsMoving = digitalRead(isMoving);
-  //if (!valueOfIsMoving) {
-    //Increment counter in 0ne second
-    timeCounter = timeCounter + 1;
-    //Write in bluethooth module the value
-    writeTimeCounterString();
-    //Only test. We read the counter value from Bluetooth
-    readTimeCounter();
-    //We print the counter value to test that the value readed fron the phone is well readed. (The same that 
-    //printed)
-    Serial.print("CounterInCharacteristic: ");
-    Serial.println(timeCounter);
-  //}
-  //We delay one second to increment counter
-  delay(1000);
+  if (IMU.gyroscopeAvailable()) {
+    //Read acceleration
+    IMU.readGyroscope(x, y, z);
+    //Serial.print("Acceleration:   ");
+    //Serial.print(x);
+    //Serial.print("     Old:   ");
+    //Serial.println(antX);
+    //Serial.println(int(x) != int(antX));
+    //If actual read is direrent of old read, bike is moving
+    if (int(x) != int(antX)) {
+      //Put actual read as old
+      antX = x;
+      //Increment counter in one second
+      timeCounter = timeCounter + 1;
+      //Write in bluethooth module the value
+      writeTimeCounterString();
+      //Only test. We read the counter value from Bluetooth
+      readTimeCounter();
+      //We print the counter value to test that the value readed fron the phone is well readed. (The same that 
+      //printed)
+      //Serial.print("CounterInCharacteristic: ");
+      //Serial.println(timeCounter);
+      //We delay one second to next reading
+    delay(1000);
+    }
+  }
 } //end loop()
